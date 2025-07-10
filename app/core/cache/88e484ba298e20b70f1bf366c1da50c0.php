b479bbf3bb1036f19ce0734a584c7426b6b9cd0a
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2025-07-10 10:50:01 -0400

    up

--- app/core/cache/88e484ba298e20b70f1bf366c1da50c0.php
+++ app/core/cache/88e484ba298e20b70f1bf366c1da50c0.php
@@ -1,93 +1,89 @@
-
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9">
-    <link rel="stylesheet" href="https://saludresponde.minsal.cl/wp-content/themes/gobcl-wp-master2/css/ie.css">
-<![endif]-->
-<!--[if gt IE 8]><!--> 
-<html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Hospital San José de Melipilla</title>
+<html class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Hospital San José de Melipilla</title>
 
-        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/main.css'>
-        <link rel="shortcut icon" type="image/x-icon" href="https://saludresponde.minsal.cl/wp-content/themes/gobcl-wp-master2/img/gob_favicon.ico"  />
+    <!-- Fonts and Icons -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" href="https://saludresponde.minsal.cl/wp-content/themes/gobcl-wp-master2/img/gob_favicon.ico" type="image/x-icon" />
 
-        <!-- Facebook -->
+    <!-- Meta Open Graph -->
+    <meta property="og:title" content="" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://saludresponde.minsal.cl/" />
+    <meta property="og:image" content="https://saludresponde.minsal.cl/wp-content/uploads/2020/01/banner-plan-de-accion-coronavirus_660x220-150x150.png" />
+    <meta property="og:site_name" content="Salud Responde">
+    <meta property="og:description" content="&nbsp;">
 
+    <!-- Robots & Prefetch -->
+    <meta name='robots' content='max-image-preview:large' />
+    <link rel='dns-prefetch' href='//ajax.googleapis.com' />
+    <link rel='dns-prefetch' href='//www.googletagmanager.com' />
+    <link rel='dns-prefetch' href='//s.w.org' />
 
-	<meta property="og:title" content="" />
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://saludresponde.minsal.cl/" />
-	<meta property="og:image" content="https://saludresponde.minsal.cl/wp-content/uploads/2020/01/banner-plan-de-accion-coronavirus_660x220-150x150.png" />
-	<meta property="og:site_name" content="Salud Responde">
-	<meta property="og:description" content="&nbsp;">
+    <!-- Styles -->
+    <link rel="stylesheet" href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/main.css'>
+    <link rel="stylesheet" href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/style.main.css'>
+    <link rel="stylesheet" href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/font-awesome.min.css'>
+    <link rel="stylesheet" href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/bootstrap-front.css'>
+    <link rel="stylesheet" href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/animate.css'>
+    <link rel="stylesheet" href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/icon-picker.css'>
+    <link rel="stylesheet" href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/glyphicon.css'>
+    <link rel="stylesheet" href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/dashicons.css'>
+    <link rel="stylesheet" href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/jquery.mCustomScrollbar.css'>
+    <link rel="stylesheet" href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/swpm.common.css'>
+    <link rel="stylesheet" href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/style.min.css'>
+    <link rel="stylesheet" href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/default.min.css'>
 
+    <!-- Scripts -->
+    <script src='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>js/jquery.min.js'></script>
+    <script src='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>js/jquery.mCustomScrollbar.concat.min.js'></script>
+    <script src='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>js/frontend.js'></script>
 
+    <!-- Extra Scripts -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
 
-<!-- ToDo: Twitter -->
-        <meta name='robots' content='max-image-preview:large' />
-<link rel='dns-prefetch' href='//ajax.googleapis.com' />
-<link rel='dns-prefetch' href='//www.googletagmanager.com' />
-<link rel='dns-prefetch' href='//s.w.org' />
-		<style type="text/css">
-img.wp-smiley,
-img.emoji {
-	display: inline !important;
-	border: none !important;
-	box-shadow: none !important;
-	height: 1em !important;
-	width: 1em !important;
-	margin: 0 .07em !important;
-	vertical-align: -0.1em !important;
-	background: none !important;
-	padding: 0 !important;
-}
-</style>
-<link rel='stylesheet' id='wp-block-library-css'  href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/style.main.css' type='text/css' media='all' />
-<link rel='stylesheet' id='wpsm_ac_pro-font-awesome-front-css'  href='<?php echo htmlspecialchars($_ENN["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/font-awesome.min.css' type='text/css' media='all' />
-<link rel='stylesheet' id='wpsm_ac_pro_bootstrap-front-css'  href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/bootstrap-front.css' type='text/css' media='all' />
-<link rel='stylesheet' id='wpsm_ac_pro_animate-css'  href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/animate.css' type='text/css' media='all' />
-<link rel='stylesheet' id='wpsm_ac_front_font-icon-picker_all-css'  href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/icon-picker.css' type='text/css' media='all' />
-<link rel='stylesheet' id='wpsm_ac_front_font-icon-picker-glyphicon_style-css'  href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/glyphicon.css' type='text/css' media='all' />
-<link rel='stylesheet' id='wpsm_ac_front_font-icon-picker-dashicons_style-css'  href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/dashicons.css' type='text/css' media='all' />
-<link rel='stylesheet' id='wpsm_ac_pro_scrollbar_style-css'  href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/jquery.mCustomScrollbar.css' type='text/css' media='all' />
-<link rel='stylesheet' id='swpm.common-css'  href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/swpm.common.css' type='text/css' media='all' />
-<link rel='stylesheet' id='pojo-a11y-css'  href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/style.min.css' type='text/css' media='all' />
-<link rel='stylesheet' id='tablepress-default-css'  href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/default.min.css' type='text/css' media='all' />
-<script type='text/javascript' src='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>js/jquery.min.js' id='jquery-js'></script>
-<script type='text/javascript' src='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>js/jquery.mCustomScrollbar.concat.min.js' id='wpsm_ac_pro_scrollbar_script-js'></script>
-<script type='text/javascript' src='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>js/frontend.js' id='stop-user-enumeration-js'></script>
+    <!-- WordPress and oEmbed Links -->
+    <link rel="https://api.w.org/" href="https://saludresponde.minsal.cl/wp-json/" />
+    <link rel="alternate" type="application/json" href="https://saludresponde.minsal.cl/wp-json/wp/v2/pages/49" />
+    <link rel="alternate" type="application/json+oembed" href="https://saludresponde.minsal.cl/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fsaludresponde.minsal.cl%2F" />
+    <link rel="alternate" type="text/xml+oembed" href="https://saludresponde.minsal.cl/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fsaludresponde.minsal.cl%2F&amp;format=xml" />
 
-<!-- Google Analytics snippet added by Site Kit -->
-<!--<script type='text/javascript' src='https://www.googletagmanager.com/gtag/js?id=GT-NGBPB78' id='google_gtagjs-js' async></script>
-<script type='text/javascript' id='google_gtagjs-js-after'>
-window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}
-gtag('set', 'linker', {"domains":["saludresponde.minsal.cl"]} );
-gtag("js", new Date());
-gtag("set", "developer_id.dZTNiMT", true);
-gtag("config", "GT-NGBPB78");
-</script>-->
+    <!-- Inline Styles for Compatibility -->
+    <style>
+        img.wp-smiley, img.emoji {
+            display: inline !important;
+            border: none !important;
+            box-shadow: none !important;
+            height: 1em !important;
+            width: 1em !important;
+            margin: 0 .07em !important;
+            vertical-align: -0.1em !important;
+            background: none !important;
+            padding: 0 !important;
+        }
+    </style>
 
-<!-- End Google Analytics snippet added by Site Kit -->
-<link rel="https://api.w.org/" href="https://saludresponde.minsal.cl/wp-json/" /><link rel="alternate" type="application/json" href="https://saludresponde.minsal.cl/wp-json/wp/v2/pages/49" /><link rel="alternate" type="application/json+oembed" href="https://saludresponde.minsal.cl/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fsaludresponde.minsal.cl%2F" />
-<link rel="alternate" type="text/xml+oembed" href="https://saludresponde.minsal.cl/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fsaludresponde.minsal.cl%2F&#038;format=xml" />
-<meta name="generator" content="Site Kit by Google 1.120.0" /> <style> .ppw-ppf-input-container { background-color: !important; padding: px!important; border-radius: px!important; } .ppw-ppf-input-container div.ppw-ppf-headline { font-size: px!important; font-weight: !important; color: !important; } .ppw-ppf-input-container div.ppw-ppf-desc { font-size: px!important; font-weight: !important; color: !important; } .ppw-ppf-input-container label.ppw-pwd-label { font-size: px!important; font-weight: !important; color: !important; } div.ppwp-wrong-pw-error { font-size: px!important; font-weight: !important; color: #dc3232!important; background: !important; } .ppw-ppf-input-container input[type='submit'] { color: !important; background: !important; } .ppw-ppf-input-container input[type='submit']:hover { color: !important; background: !important; } .ppw-ppf-desc-below { font-size: px!important; font-weight: !important; color: !important; } </style>  <style> .ppw-form { background-color: !important; padding: px!important; border-radius: px!important; } .ppw-headline.ppw-pcp-pf-headline { font-size: px!important; font-weight: !important; color: !important; } .ppw-description.ppw-pcp-pf-desc { font-size: px!important; font-weight: !important; color: !important; } .ppw-pcp-pf-desc-above-btn { display: block; } .ppw-pcp-pf-desc-below-form { font-size: px!important; font-weight: !important; color: !important; } .ppw-input label.ppw-pcp-password-label { font-size: px!important; font-weight: !important; color: !important; } .ppw-form input[type='submit'] { color: !important; background: !important; } .ppw-form input[type='submit']:hover { color: !important; background: !important; } div.ppw-error.ppw-pcp-pf-error-msg { font-size: px!important; font-weight: !important; color: #dc3232!important; background: !important; } </style> <style type="text/css">
-#pojo-a11y-toolbar .pojo-a11y-toolbar-toggle a{ background-color: #4054b2;	color: #ffffff;}
-#pojo-a11y-toolbar .pojo-a11y-toolbar-overlay, #pojo-a11y-toolbar .pojo-a11y-toolbar-overlay ul.pojo-a11y-toolbar-items.pojo-a11y-links{ border-color: #4054b2;}
-body.pojo-a11y-focusable a:focus{ outline-style: solid !important;	outline-width: 1px !important;	outline-color: #FF0000 !important;}
-#pojo-a11y-toolbar{ top: 100px !important;}
-#pojo-a11y-toolbar .pojo-a11y-toolbar-overlay{ background-color: #ffffff;}
-#pojo-a11y-toolbar .pojo-a11y-toolbar-overlay ul.pojo-a11y-toolbar-items li.pojo-a11y-toolbar-item a, #pojo-a11y-toolbar .pojo-a11y-toolbar-overlay p.pojo-a11y-toolbar-title{ color: #333333;}
-#pojo-a11y-toolbar .pojo-a11y-toolbar-overlay ul.pojo-a11y-toolbar-items li.pojo-a11y-toolbar-item a.active{ background-color: #4054b2;	color: #ffffff;}
-@media (max-width: 767px) { #pojo-a11y-toolbar { top: 50px !important; } }</style>
-
-    </head>
-    <body class="home page-template-default page page-id-49">
+    <!-- Custom UI Styling -->
+    <style>
+        /* Simplified version of accessibility and password protection styles */
+        #pojo-a11y-toolbar .pojo-a11y-toolbar-toggle a {
+            background-color: #4054b2;
+            color: #ffffff;
+        }
+        body.pojo-a11y-focusable a:focus {
+            outline: 1px solid #FF0000 !important;
+        }
+        @media (max-width: 767px) {
+            #pojo-a11y-toolbar { top: 50px !important; }
+        }
+    </style>
+</head>
+<body class="home page-template-default page page-id-49">
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.js"></script>  
 	<script type="text/javascript" src="https://code.jquery.com/ui/1.13.0/jquery-ui.js" ></script>
 
@@ -139,77 +135,71 @@ body.pojo-a11y-focusable a:focus{ outline-style: solid !important;	outline-width
         </div>
     </div>
 
-    <header style="background-image:url('https://saludresponde.minsal.cl/wp-content/uploads/2025/02/header-web-cabecera_ges-20-an╠aos-1920x1440-1.png')">
-        <div class="wrap">
-			
-			<!--<h1 id="logo-main">
-                <a href="#" target=_blank>
-                    <img src="https://saludresponde.minsal.cl/wp-content/uploads/2020/07/logo.png">
-                </a>
-            </h1>-->
-			
-<script type="text/javascript">
-window.____aParams = {"gobabierto":"1","buscadore":"1","icons":"1"};
-(function() {
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-})();
-</script>
-		<nav id="menu-principal">
-			<!-- Menu Principal -->
-			<ul id="menu-main-menu" class="menu-main">
-				<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-48">
-					<a href="#">Inicio</a>
-				</li>
-				<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-31">
-					<a>Salud Responde</a>
+<header style="background-image:url('https://saludresponde.minsal.cl/wp-content/uploads/2025/02/header-web-cabecera_ges-20-an╠aos-1920x1440-1.png')">
+<div class="wrap">
+	
+	<!--<h1 id="logo-main">
+		<a href="#" target=_blank>
+			<img src="https://saludresponde.minsal.cl/wp-content/uploads/2020/07/logo.png">
+		</a>
+	</h1>-->
+		
+	<nav id="menu-principal">
+		<!-- Menu Principal -->
+		<ul id="menu-main-menu" class="menu-main">
+			<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-48">
+				<a href="#">Inicio</a>
+			</li>
+			<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-31">
+				<a>Salud Responde</a>
+				<ul class="sub-menu">
+					<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-6068">
+						<a href="#">Qué es Salud Responde</a>
+					</li>
+				</ul>
+			</li>
+			<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-32">
+				<a href="#">Información Administrativa</a>
+				<ul class="sub-menu">
+					<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-20032">
+						<a href="#">Ley N°21.746 modifica normativa sobre licencias médicas</a>
+					</li>
+				</ul>
+			</li>
+			<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-38">
+				<a>Preguntas Frecuentes</a>
+				<ul class="sub-menu">
+					<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-20002">
+						<a href="#">Puesta al día Vacunación VPH y Sarampión</a>
+					</li>
 					<ul class="sub-menu">
-						<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-6068">
-							<a href="#">Qué es Salud Responde</a>
+						<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3464">
+							<a href="#">Formularios PNAC</a>
 						</li>
 					</ul>
-				</li>
-				<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-32">
-					<a href="#">Información Administrativa</a>
-					<ul class="sub-menu">
-						<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-20032">
-							<a href="#">Ley N°21.746 modifica normativa sobre licencias médicas</a>
-						</li>
-					</ul>
-				</li>
-				<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-38">
-					<a>Preguntas Frecuentes</a>
-					<ul class="sub-menu">
-						<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-20002">
-							<a href="#">Puesta al día Vacunación VPH y Sarampión</a>
-						</li>
-						<ul class="sub-menu">
-							<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3464">
-								<a href="#">Formularios PNAC</a>
-							</li>
-						</ul>
-						<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-19963">
-							<a href="#">Mycoplasma pneumoniae</a>
-						</li>
-					</ul>
-				</li>
-				<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-33">
-					<a>Difusión</a>
-					<ul class="sub-menu">
-						<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-669">
-							<a href="#">Boletines</a>
-						</li>
-					</ul>
-				</li>
-				<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-10108">
-					<a>Intranet</a>
-					<ul class="sub-menu">
-						<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-5735">
-							<a href="#">Capacitación (acceso solo funcionarios)</a>
-						</li>
-					</ul>
-				</li>
-			</ul>         
-		</nav>
-        	<a href="#" id="menu-movil-trigger">Menú Principal</a>
-        </div>
-    </header>
+					<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-19963">
+						<a href="#">Mycoplasma pneumoniae</a>
+					</li>
+				</ul>
+			</li>
+			<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-33">
+				<a>Difusión</a>
+				<ul class="sub-menu">
+					<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-669">
+						<a href="#">Boletines</a>
+					</li>
+				</ul>
+			</li>
+			<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-10108">
+				<a>Intranet</a>
+				<ul class="sub-menu">
+					<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-5735">
+						<a href="#">Capacitación (acceso solo funcionarios)</a>
+					</li>
+				</ul>
+			</li>
+		</ul>         
+	</nav>
+		<a href="#" id="menu-movil-trigger">Menú Principal</a>
+	</div>
+</header>
