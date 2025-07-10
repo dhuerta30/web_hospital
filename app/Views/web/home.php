ae3023e08a83e2eca6de903c975f591956ac5589
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2025-07-10 12:13:04 -0400

    up

--- app/Views/web/home.php
+++ app/Views/web/home.php
@@ -259,52 +259,84 @@
             </div>
             <div class="banners">
                 <div class="banner banner-corto">
-                    <a href="https://portalsaluddigital.minsal.cl/"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2025/07/banners-320x100-1.png" alt="Portal Salud Digital "></a>
+                    <a href="https://portalsaluddigital.minsal.cl/">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2025/07/banners-320x100-1.png" alt="Portal Salud Digital ">
+                    </a>
                 </div>
                 <div class="banner banner-corto">
-                    <a href="https://saludresponde.minsal.cl/atencion-de-salud-en-lengua-de-senas-chilena/"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2023/09/banner-lengua-de-senas-01.png" alt="Atención remota vía lengua de señas"></a>
+                    <a href="https://saludresponde.minsal.cl/atencion-de-salud-en-lengua-de-senas-chilena/">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2023/09/banner-lengua-de-senas-01.png" alt="Atención remota vía lengua de señas">
+                    </a>
                 </div>
                 <div class="banner banner-corto">
-                    <a href="https://saludresponde.minsal.cl/atencion-de-salud-en-kreyol/"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2023/10/banner-kreyol-2-1.png" alt="Atención en Kreyol"></a>
+                    <a href="https://saludresponde.minsal.cl/atencion-de-salud-en-kreyol/">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2023/10/banner-kreyol-2-1.png" alt="Atención en Kreyol">
+                    </a>
                 </div>
                 <div class="banner banner-corto">
-                    <a href="https://saludresponde.minsal.cl/agendamiento-y-atencion-en-cuidados-paliativos/"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2024/08/banner-agendamiento-cuidados-paliativos-Dependencia-Severa-LATERAL.png" alt=""></a>
+                    <a href="https://saludresponde.minsal.cl/agendamiento-y-atencion-en-cuidados-paliativos/">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2024/08/banner-agendamiento-cuidados-paliativos-Dependencia-Severa-LATERAL.png" alt="">
+                    </a>
                 </div>
                 <div class="banner banner-corto">
-                    <a href="https://seremienlinea.minsal.cl/asdigital/index.php?mfarmacias"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2023/10/Farmacias-2023.png" alt="Farmacias de turno, horario normal y más"></a>
+                    <a href="https://seremienlinea.minsal.cl/asdigital/index.php?mfarmacias">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2023/10/Farmacias-2023.png" alt="Farmacias de turno, horario normal y más">
+                    </a>
                 </div>
                 <div class="banner banner-corto">
-                    <a href="https://saludresponde.minsal.cl/buscador-de-establecimientos-de-salud/"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2023/09/visor-territorial-salud-respondev2.png" alt="búsqueda establecimientos de salud"></a>
+                    <a href="https://saludresponde.minsal.cl/buscador-de-establecimientos-de-salud/">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2023/09/visor-territorial-salud-respondev2.png" alt="búsqueda establecimientos de salud">
+                    </a>
                 </div>
                 <div class="banner banner-corto">
-                    <a href="https://saludresponde.minsal.cl/vacunacion-sarampion/"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2025/06/banner-lateral_sarampion-2025.png" alt=""></a>
+                    <a href="https://saludresponde.minsal.cl/vacunacion-sarampion/">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2025/06/banner-lateral_sarampion-2025.png" alt="">
+                    </a>
                 </div>
                 <div class="banner banner-corto">
-                    <a href="https://saludresponde.minsal.cl/estrategia-de-vacunacion-escolar-2025/"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2025/06/vacunacion-escolar-2025-01.png" alt=""></a>
+                    <a href="https://saludresponde.minsal.cl/estrategia-de-vacunacion-escolar-2025/">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2025/06/vacunacion-escolar-2025-01.png" alt="">
+                    </a>
                 </div>
                 <div class="banner banner-corto">
-                    <a href="https://saludresponde.minsal.cl/vacunate-contra-la-tos-convulsiva-preguntas-frecuentes/"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2025/06/banner-vacuna-tos-convulsiva-embarazadas.png" alt=""></a>
+                    <a href="https://saludresponde.minsal.cl/vacunate-contra-la-tos-convulsiva-preguntas-frecuentes/">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2025/06/banner-vacuna-tos-convulsiva-embarazadas.png" alt="">
+                    </a>
                 </div>
                 <div class="banner banner-corto">
-                    <a href="https://www.remediosmasbaratos.cl/"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2024/04/banner-lateral_remedios-mas-baratos-cenabast-1.png" alt="Remedios más baratos ley cenabast"></a>
+                    <a href="https://www.remediosmasbaratos.cl/">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2024/04/banner-lateral_remedios-mas-baratos-cenabast-1.png" alt="Remedios más baratos ley cenabast">
+                    </a>
                 </div>
                 <div class="banner banner-corto">
-                    <a href="https://portalsaluddigital.minsal.cl/"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2023/09/LINEA-PREVENCION-DEL-SUICIDIO-4141.png" alt="Línea de Prevención del Suicidio"></a>
+                    <a href="https://portalsaluddigital.minsal.cl/">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2023/09/LINEA-PREVENCION-DEL-SUICIDIO-4141.png" alt="Línea de Prevención del Suicidio">
+                    </a>
                 </div>
                 <div class="banner banner-corto">
-                    <a href="https://saludresponde.minsal.cl/vacuna-escolar-contra-el-vph/"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2024/08/banner-vacuna-nonavalente-lat.png" alt="etiquetado alcoholes"></a>
+                    <a href="https://saludresponde.minsal.cl/vacuna-escolar-contra-el-vph/">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2024/08/banner-vacuna-nonavalente-lat.png" alt="etiquetado alcoholes">
+                    </a>
                 </div>
                 <div class="banner banner-corto">
-                    <a href="https://saludresponde.minsal.cl/autotest-vih/"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2023/09/banner-lateral_autotest-vih.png" alt="Autotes VIH"></a>
+                    <a href="https://saludresponde.minsal.cl/autotest-vih/">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2023/09/banner-lateral_autotest-vih.png" alt="Autotes VIH">
+                    </a>
                 </div>
                 <div class="banner banner-corto">
-                    <a href="https://saludresponde.minsal.cl/ley-ive-y-salud-responde/"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2024/03/LEY-IVE.png" alt="Interrupción voluntaria del embarazo"></a>
+                    <a href="https://saludresponde.minsal.cl/ley-ive-y-salud-responde/">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2024/03/LEY-IVE.png" alt="Interrupción voluntaria del embarazo">
+                    </a>
                 </div>
                 <div class="banner banner-corto">
-                    <a href="https://saludresponde.minsal.cl/universalizacion-de-la-atencion-primaria-de-salud/"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2023/09/APS-UNiversal-lateral.png" alt="Universalización de la Atención Primaria"></a>
+                    <a href="https://saludresponde.minsal.cl/universalizacion-de-la-atencion-primaria-de-salud/">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2023/09/APS-UNiversal-lateral.png" alt="Universalización de la Atención Primaria">
+                    </a>
                 </div>
                 <div class="banner banner-corto">
-                    <a href="https://saludresponde.minsal.cl/viruela-del-mono-monkeypox/"><img src="https://saludresponde.minsal.cl/wp-content/uploads/2024/08/banner-lateral-MPOX.jpg" alt="Portal Paciente"></a>
+                    <a href="https://saludresponde.minsal.cl/viruela-del-mono-monkeypox/">
+                        <img src="https://saludresponde.minsal.cl/wp-content/uploads/2024/08/banner-lateral-MPOX.jpg" alt="Portal Paciente">
+                    </a>
                 </div>
             </div>
             <div class="clearfix"></div>
