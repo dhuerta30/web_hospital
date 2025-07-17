f4cab05a45693b38630e8a12c1344b197e78e01b
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2025-07-17 10:22:02 -0400

    up

--- app/Models/NoticiasModel.php
+++ app/Models/NoticiasModel.php
@@ -26,7 +26,7 @@ class NoticiasModel
         $artify->setSettings("refresh", false);
         $artify->setSettings("function_filter_and_search", true);
         $artify->addCallback("format_table_data", [$this, "formatearDatosTablaNoticias"]);
-        $artify->crudTableCol(array("titulo","fecha","imagen"));
+        $artify->crudTableCol(array("titulo", "fecha","imagen", "contenido"));
         $artify->setSettings("template", $this->table);
         $render = $artify->dbTable($this->table)->render();
         return $render;
@@ -37,7 +37,8 @@ class NoticiasModel
             foreach($data as &$item){
                 $item["titulo"] = "<center><h3><strong>".$item["titulo"]."</strong></h3></center>";
                 $item["fecha"] = "<center><h5>".date("d/m/Y", strtotime($item["fecha"]))."</h5></center>";
-                 $item["imagen"] = "<img width='100%' src='".$_ENV["BASE_URL"]."app/libs/artify/uploads/".$item["imagen"]."'>";
+                $item["imagen"] = "<img width='100%' src='".$_ENV["BASE_URL"]."app/libs/artify/uploads/".$item["imagen"]."'>";
+                $item["contenido"] = mb_strimwidth(strip_tags(html_entity_decode($item["contenido"], ENT_QUOTES, 'UTF-8')), 0, 250, "...");
             }
         }
         return $data;
